6140908ae62f558c7ebac2423ae36f12ce9474da
MMartinez2820 <201808724@p.uapa.edu.do>
2022-11-06 03:24:23 -0400

    four

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Locations from './components/Locations'
+
+import './App.css'
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Locations/>
+      <footer>
+        <h3>DEVELOPERS</h3>
+        <p>Paul Ruiz</p>
+        <p>Miguel Martinez</p>
+        <p>Robert </p>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
